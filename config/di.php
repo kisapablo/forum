dc17134f3879aed4725e59e583d70c58191a772a
Paval-from-Belarus <star-war-sandakan@mail.ru>
2025-08-17 14:24:53 +0300

    update di and env configs

--- config/di.php
+++ config/di.php
@@ -6,15 +6,23 @@ use Twig\Loader\FilesystemLoader;
 use function DI\autowire;
 use function DI\get;
 
+$host = getenv("DATABASE_HOST");
+
+if ($host == null) {
+    $host = "localhost";
+}
+
+$dsn = "mysql:host=" . $host . ";dbname=" . getenv("DATABASE_NAME") . ";charset=utf8mb4";
+
 return [
     FilesystemLoader::class => autowire()
-    ->constructorParameter('paths', 'Templates'),
+        ->constructorParameter('paths', 'Templates'),
 
     Environment::class => autowire()
-    ->constructorParameter('loader', get(FilesystemLoader::class)),
+        ->constructorParameter('loader', get(FilesystemLoader::class)),
 
     DataBase::class => autowire()
-    ->constructorParameter('dsn', getenv('DATABASE_DSN'))
-    ->constructorParameter('username', getenv('DATABASE_USERNAME'))
-    ->constructorParameter('password', getenv('DATABASE_PASSWORD'))
+        ->constructorParameter('dsn', $dsn)
+        ->constructorParameter('username', getenv('DATABASE_USERNAME'))
+        ->constructorParameter('password', getenv('DATABASE_PASSWORD'))
 ];
